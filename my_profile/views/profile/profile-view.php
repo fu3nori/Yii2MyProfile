07a18bee1457323271678eca2753165a4742df91
LAPTOP-8VU5K4S1\NCJ_0 <cap.fuminori.usui@gmail.com>
2024-07-08 16:06:37 +0900

    シェアボタン追加

--- my_profile/views/profile/profile-view.php
+++ my_profile/views/profile/profile-view.php
@@ -11,6 +11,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 $profileUrl = Yii::$app->urlManager->createAbsoluteUrl(['profile/view', 'id' => $profile->user_id]);
 $domain = Yii::$app->request->hostInfo; // ドメイン名を取得
+
 ?>
 
 <div class="profile-view container">
@@ -20,7 +21,7 @@ $domain = Yii::$app->request->hostInfo; // ドメイン名を取得
     <div class="row">
         <div class="col-12">
             <p>URL: <?= Html::a($profileUrl, $profileUrl) ?></p>
-            <?= Html::button('URLをコピーする', ['class' => 'btn btn-primary', 'onclick' => 'copyToClipboard("'.$profileUrl.'")']) ?>
+            <?= Html::button('URLをコピーする', ['class' => 'btn btn-primary', 'onclick' => "copyToClipboard(\"$profileUrl\")"]) ?>
         </div>
     </div>
 
@@ -33,10 +34,25 @@ $domain = Yii::$app->request->hostInfo; // ドメイン名を取得
 
     <div class="row mt-3">
         <div class="col-12">
+
             <p>プロフィールをシェアする:</p>
+            <!-- twitter -->
             <a href="https://twitter.com/intent/tweet?url=<?= urlencode($profileUrl) ?>&text=<?= urlencode('これが私のプロフィールです') ?>" class="btn btn-black text-white twitter-share" target="_blank">
                 X(Twitterでシェア)
-            </a>
+            </a><br>
+            <!-- ここまで -->
+            <!-- facebook -->
+            <br>
+            <div id="fb-root"></div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v20.0" nonce="ClZ293sE"></script>
+            <div class="fb-share-button" data-href="" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?= urlencode($profileUrl) ?>;src=sdkpreparse" class="fb-xfbml-parse-ignore">シェアする</a></div>
+            <!-- ここまで-->
+            <br> <br>
+            <!-- LINE -->
+            <button class="btn-line" onclick="shareToLine()">LINEでシェア</button>
+
+        </div>
+            <!-- ここまで -->
         </div>
     </div>
 
@@ -51,7 +67,7 @@ $domain = Yii::$app->request->hostInfo; // ドメイン名を取得
     <?php if ($profile->thum_url1 || $profile->thum_url2 || $profile->thum_url3 || $profile->thum_url4 || $profile->thum_url5): ?>
         <div class="row mt-3">
             <div class="col-12">
-                <h2 class="display-6">ポートフォリオ・画像</h2>
+                <h2 class="display-6">Image</h2>
                 <div class="heading-underline mb-3"></div>
             </div>
             <?php for ($i = 1; $i <= 5; $i++): ?>
@@ -103,10 +119,10 @@ $domain = Yii::$app->request->hostInfo; // ドメイン名を取得
                             <td>
                                 <?php if ($isLink): ?>
                                     <?= Html::a(Html::encode($displayUrl), $url, ['target' => '_blank', 'title' => Html::encode($url)]) ?><br>
-                                    <?= Html::button('クリップボードにコピー', ['class' => 'btn btn-secondary btn-sm mt-1', 'onclick' => 'copyToClipboard("'.$url.'")']) ?>
+                                    <?= Html::button('クリップボードにコピー', ['class' => 'btn btn-secondary btn-sm mt-1', 'onclick' => "copyToClipboard(\"$url\")"]) ?>
                                 <?php else: ?>
                                     <?= Html::encode($displayUrl) ?><br>
-                                    <?= Html::button('クリップボードにコピー', ['class' => 'btn btn-secondary btn-sm mt-1', 'onclick' => 'copyToClipboard("'.$url.'")']) ?>
+                                    <?= Html::button('クリップボードにコピー', ['class' => 'btn btn-secondary btn-sm mt-1', 'onclick' => "copyToClipboard(\"$url\")"]) ?>
                                 <?php endif; ?>
                             </td>
                         </tr>
@@ -115,10 +131,10 @@ $domain = Yii::$app->request->hostInfo; // ドメイン名を取得
                                 <strong><?= Html::encode($service) ?></strong><br>
                                 <?php if ($isLink): ?>
                                     <?= Html::a(Html::encode($displayUrl), $url, ['target' => '_blank', 'title' => Html::encode($url)]) ?><br>
-                                    <?= Html::button('クリップボードにコピー', ['class' => 'btn btn-secondary btn-sm mt-1', 'onclick' => 'copyToClipboard("'.$url.'")']) ?>
+                                    <?= Html::button('クリップボードにコピー', ['class' => 'btn btn-secondary btn-sm mt-1', 'onclick' => "copyToClipboard(\"$url\")"]) ?>
                                 <?php else: ?>
                                     <?= Html::encode($displayUrl) ?><br>
-                                    <?= Html::button('クリップボードにコピー', ['class' => 'btn btn-secondary btn-sm mt-1', 'onclick' => 'copyToClipboard("'.$url.'")']) ?>
+                                    <?= Html::button('クリップボードにコピー', ['class' => 'btn btn-secondary btn-sm mt-1', 'onclick' => "copyToClipboard(\"$url\")"]) ?>
                                 <?php endif; ?>
                             </td>
                         </tr>
@@ -136,8 +152,14 @@ $domain = Yii::$app->request->hostInfo; // ドメイン名を取得
             alert('クリップボードにコピーしました: ' + text);
         });
     }
-</script>
 
+    function shareToLine() {
+        var url = "<?= $profileUrl ?>";
+        var text = "私のプロフィールはこちら " + url;
+        var lineUrl = 'https://line.me/R/msg/text/?' + encodeURIComponent(text);
+        window.open(lineUrl, '_blank');
+    }
+</script>
 <style>
     .btn-black {
         background-color: #000;
@@ -164,6 +186,17 @@ $domain = Yii::$app->request->hostInfo; // ドメイン名を取得
     .twitter-share:hover {
         background-color: #0d8ddb;
         color: white;
+    }
+    .btn-line {
+        background-color: #00B900;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+    .btn-line:hover {
+        background-color: #009900;
     }
     @media (max-width: 767.98px) {
         .d-md-table-row {
