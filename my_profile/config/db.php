09eea5f88ccc54e55ce211e3e1468a59846e77d4
fu3nori <7250837+fu3nori@users.noreply.github.com>
2024-12-17 10:26:46 +0900

    Create db.php
    db.php初期化

---
+++ my_profile/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=HOSTNAME;dbname=DB_NAME',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
