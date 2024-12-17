40b74f169d40a99567af15d427e7a41f72ffc167
fu3nori <cap.fuminori.usui@gmail.com>
2024-12-17 10:17:20 +0900

    DB設定をサンプル用に設定

--- my_profile/config/db.php
+++ my_profile/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2_my_profile',
+    'dsn' => 'mysql:host=HOSTNAME;dbname=DB_NAME',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
